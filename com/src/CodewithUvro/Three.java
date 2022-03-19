5e3175f39f5532c65c7621c830f7bb7cae73bc27
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-19 13:09:16 +0530

    Create Three.java

---
+++ com/src/CodewithUvro/Three.java
@@ -0,0 +1,42 @@
+package CodewithUvro;
+
+public class Three {
+    public static void main(String[] args) {
+        // Search in a sorted matrix
+        // ie all the elements are sorted in a row wise format
+        int[][] arr = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+        // this is a sorted matrix
+    }
+}
+// points :
+// 1st element of a particular row is > the last element of the previous row
+//
+// Intersting technique -- > convert the 2d matrix into a 1d array and search for the element
+// but
+// we are going to search for the target element by using binary search
+
+// IMPORTANT TIP- TRY TO REDUCE THE SEARCH SPACE BY ELIMINATING ROWS AND COLUMNS
+
+// APPROACH -
+// 1] WE ARE TAKING THE MIDDLE COLUMN AS THE MATRIX IS SORTED
+// AND WE ARE DOING BINARY SEARCH ON IT IN ORDER TO REDUCE NO. OF ROWS
+// OR ] ALTERNATE APPROACH WOULD BE TO TAKE MIDDLE ROW AND PERFORM BS
+// IN ORDER TO REDUCE THE NO. OF COLUMNS .
+
+// CASE 1 : IF ELEMENT == TARGET --> ANS FOUND
+// CASE 2 : IF ELEMENT > TARGET
+// THEN ALL THE ROWS AFTER THE ELEMENT WILL BE IGNORED
+// CASE 3 : IF ELEMENT < TARGET
+// THEN  IGNORE ABOVE ROWS
+// THIS is how we are reducing the search space
+
+// IN THE END
+// in the end there will be 2 rows remaining
+// STEPS :
+// 1]CHECK WHETHER THE MID COL YOU ARE AT CONTAINS    THE TARGET ELEMENT
+// 2] CONSIDER THE 4 PARTS 
